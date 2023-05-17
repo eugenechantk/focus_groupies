55063bed656bc496e6bd1dd3906b28f27ed8a56b
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2023-05-16 19:19:07 -0700

    modified content.js to show agent status bar

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,9 @@
 import { printLine } from "./modules/print";
 import logo from "../../assets/img/logo.svg";
 import cursor from "../../assets/img/cursor.svg";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import AgentStatusContainer from "./components/AgentStatus/AgentStatusContainer"
 
 console.log("Content script works!");
 console.log("Must reload extension for modifications to take effect.");
@@ -10,3 +13,19 @@ chrome.runtime.sendMessage({ greeting: "hello" }, function (response) {
 });
 
 printLine("Using the 'printLine' function from the Print Module");
+
+const body = document.querySelector('body')
+const app = document.createElement('div')
+app.style.cssText = 'z-index:10;position:absolute;bottom:16px;width:100%'
+
+app.id = 'react-root'
+
+if (body) {
+  body.prepend(app)
+}
+
+const container = document.getElementById('react-root');
+const root = createRoot(container);
+root.render(
+    <AgentStatusContainer />
+);
