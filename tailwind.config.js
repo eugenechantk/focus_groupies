8e099920d2fc2dec10897f8dcd9de269eb2f22a4
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2023-05-16 18:21:02 -0700

    customized colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    fontFamily:{
+      'mono': ['"Source Code Pro"', ...defaultTheme.fontFamily.mono],
+    },
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'theme': {
+          100: '#FFFFFF',
+          90: '#E5E5E5',
+          25: '#404040',
+          12: '#1F1F1F',
+          5: '#1D1D1D',
+        }
+      }
+    },
   },
   plugins: [],
 };
