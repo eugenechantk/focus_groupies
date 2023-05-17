2897e9f6a3a665c2fa560e41ed501c56c8333707
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2023-05-16 19:18:47 -0700

    added all components needed for agent status bar and quip

---
+++ src/pages/Content/components/AgentStatus/AgentStatusContainer.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AgentStatusBar from './AgentStatusBar'
+
+export default function AgentStatusContainer() {
+  return (
+    <div>
+      <AgentStatusBar/>
+    </div>
+  )
+}
