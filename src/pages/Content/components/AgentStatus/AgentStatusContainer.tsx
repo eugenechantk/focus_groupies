b304525e0502a95713dffade1c39f60ea47f2453
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2023-05-18 02:41:44 -0700

    change play button icon to fast forward

--- src/pages/Content/components/AgentStatus/AgentStatusContainer.tsx
+++ src/pages/Content/components/AgentStatus/AgentStatusContainer.tsx
@@ -90,39 +90,20 @@ export default function AgentStatusContainer({
       {/* Status */}
       <PillContainer>
         <PlayButton onClick={setIsAgentPaused}>
-          {isAgentPaused ? (
-            // Play icon
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              style={{ width: "24px", height: "24px" }}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
-              />
-            </svg>
-          ) : (
-            // Pause icon
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              style={{ width: "24px", height: "24px" }}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15.75 5.25v13.5m-7.5-13.5v13.5"
-              />
-            </svg>
-          )}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            style={{width: "24px", height: "24px"}}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 8.688c0-.864.933-1.405 1.683-.977l7.108 4.062a1.125 1.125 0 010 1.953l-7.108 4.062A1.125 1.125 0 013 16.81V8.688zM12.75 8.688c0-.864.933-1.405 1.683-.977l7.108 4.062a1.125 1.125 0 010 1.953l-7.108 4.062a1.125 1.125 0 01-1.683-.977V8.688z"
+            />
+          </svg>
         </PlayButton>
         <AgentStatusPill
           onClick={() => setIsQuipModalOpen(!isQuipModalOpen)}
